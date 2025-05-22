c47c63db9699bddaabaf653830bb34a088bb23a0
Nikita Derevyanko <nxderevyanko@gmail.com>
2025-05-22 12:59:45 +0300

    Создание проекта

---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+import { fileURLToPath, URL } from 'node:url'
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  alias: {
+    '@': fileURLToPath(new URL('./', import.meta.url)),
+    '~': fileURLToPath(new URL('./', import.meta.url)),
+    '@assets': fileURLToPath(new URL('./assets', import.meta.url)),
+    '@components': fileURLToPath(new URL('./components', import.meta.url)),
+    '@styles': fileURLToPath(new URL('./assets/styles', import.meta.url)),
+    '@public': fileURLToPath(new URL('./public', import.meta.url)),
+  },
+  app: {
+    head: {
+      title: 'IT Sharks',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        {
+          name: 'IT Sharks',
+          content: 'Описание сайта'
+        }
+      ],
+      link: [
+        { rel: 'icon', href: '/favicon.ico' }
+      ]
+    }
+  },
+  css: ['~/assets/styles/main.scss'],
+})
